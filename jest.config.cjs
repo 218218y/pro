c20332c3bfe277b8425f6bf0bb80e56e192628b4
218218y <218yaakov@gmail.com>
2026-07-26 00:54:23 +0300

    הסרת jest

--- jest.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  verbose: true,
-};
